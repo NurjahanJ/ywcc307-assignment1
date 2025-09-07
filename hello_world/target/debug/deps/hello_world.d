fc47e9c66962c1cfcb1ae87408909bd005b44be5
Nurjahan Jhorna <nj32@njit.edu>
2025-09-07 13:48:23 -0400

    Created files and run the code

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\Nurja\OneDrive\Documents\YWCC307\ywcc307-assignment1\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+C:\Users\Nurja\OneDrive\Documents\YWCC307\ywcc307-assignment1\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\Nurja\OneDrive\Documents\YWCC307\ywcc307-assignment1\hello_world\target\debug\hello_world.exe: C:\Users\Nurja\OneDrive\Documents\YWCC307\ywcc307-assignment1\hello_world\src\main.rs
